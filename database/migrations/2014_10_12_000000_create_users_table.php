f3c31f2de93652d56325122d159a69f4ef236627
kuzmich321 <dota2drot@gmail.com>
2020-07-31 14:09:25 +0000

    * users updated * profiles added as one-to-one relationship

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
+            $table->string('name', 50);
+            $table->string('surname', 50)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();

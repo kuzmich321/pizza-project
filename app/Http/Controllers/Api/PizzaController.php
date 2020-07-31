64d6c0ae1e5306e32c659ce31b0378fafd00795d
kuzmich321 <dota2drot@gmail.com>
2020-07-31 16:21:21 +0000

    * pizzas api added

---
+++ app/Http/Controllers/Api/PizzaController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PizzaCollection;
+use Illuminate\Http\Request;
+use App\Pizza;
+
+class PizzaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pizzaQuery = Pizza::query();
+
+        $pizzas = $pizzaQuery->when(request('per_page'), function ($query, $perPage) {
+            return $query->paginate($perPage);
+        }, function ($query) {
+            return $query->get();
+        });
+
+        return new PizzaCollection($pizzas);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('Api')->name('api.')->group(function () {
+    Route::apiResource('/pizzas', 'PizzaController')->only(['index', 'show']);
+});

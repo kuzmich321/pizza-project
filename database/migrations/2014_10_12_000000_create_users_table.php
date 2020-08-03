6326a2e3cebdfd1b5bf6ca03b13df588707caf25
kuzmich321 <dota2drot@gmail.com>
2020-08-03 15:35:43 +0000

    * small fixes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->string('name', 50);
+            $table->string('name', 50)->nullable();
             $table->string('surname', 50)->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');

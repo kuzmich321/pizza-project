b9093261929b6f09fbf1546af67635de6d69b967
kuzmich321 <dota2drot@gmail.com>
2020-08-05 23:32:11 +0000

    * auth api controller added * default auth removed * api auth routes added * register request added

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterFormRequest;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        if (!$token = auth()->attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Wrong email or password. Try again.'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return response()->json(compact('token'), Response::HTTP_OK);
+    }
+
+    public function register(RegisterFormRequest $request)
+    {
+        $input = $request->validated();
+        $input['password'] = bcrypt($input['password']);
+
+        $user = User::create($input);
+
+        return response()->json([
+           'status' => true,
+           'user' => $user
+        ], Response::HTTP_OK);
+    }
+
+    public function logout()
+    {
+        return auth()->logout();
+    }
+
+    public function me(Request $request)
+    {
+        $user = $request->user();
+
+        return response()->json([
+            'id' => $user->id,
+            'email' => $user->email,
+            'name' => $user->name,
+        ], Response::HTTP_OK);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::namespace('Api')->group(function () {
+    Route::prefix('auth')->group(function() {
+        Route::post('login', 'AuthController@login');
+        Route::post('register', 'AuthController@register');
+        Route::post('logout', 'AuthController@logout')->middleware('jwt.auth');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+        Route::get('me', 'AuthController@me')->middleware('jwt.auth');
+    });
 
-Route::namespace('Api')->name('api.')->group(function () {
     Route::apiResource('/pizzas', 'PizzaController')->only(['index', 'show']);
+    Route::apiResource('/card', 'CardController')->only(['index', 'show']);
 });

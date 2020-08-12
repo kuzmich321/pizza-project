4894db4cf8329f3698993a258ed2dd4a95b39f5f
kuzmich321 <dota2drot@gmail.com>
2020-08-12 14:04:21 +0000

    * currency added

--- resources/js/components/CardComponent.vue
+++ resources/js/components/CardComponent.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="container">
+        <button class="btn btn-secondary float-right mb-4" @click.prevent="isDollars = !isDollars" v-if="pizzas.length > 0">Change Currency</button>
+
         <template v-if="pizzas.length > 0">
             <table class="table table-hover mb-5">
                 <thead>
@@ -19,7 +21,8 @@
                         </th>
                         <td class="text-center align-middle"><h4>{{ pizza.name }}</h4></td>
                         <td class="text-center align-middle">{{ pizza.quantity }}</td>
-                        <td class="text-center align-middle">{{ pizza.orderPrice }}</td>
+                        <td class="text-center align-middle" v-if="isDollars">{{ pizza.orderPrice }} $</td>
+                        <td class="text-center align-middle" v-else>{{ Math.ceil(pizza.orderPrice * 0.85) }} €</td>
                         <td class="text-center align-middle">
                             <a href="" class="btn btn-danger" @click.prevent="removeFromCard(pizza)">Delete</a>
                         </td>
@@ -28,7 +31,9 @@
             </table>
             <div class="row mb-5">
                 <div class="col-12">
-                    <h4 class="text-secondary text-right">Total Price: <span class="text-primary">{{totalPrice}}</span>
+                    <h4 class="text-secondary text-right">Total Price:
+                        <span class="text-primary" v-if="isDollars">{{totalPrice}} $</span>
+                        <span class="text-primary" v-else>{{Math.ceil(totalPrice * 0.85)}} €</span>
                     </h4>
                 </div>
             </div>
@@ -39,7 +44,7 @@
                 <div class="col-10">
                     <div class="jumbotron">
                         <form action="">
-                            <div class="form-group">
+                            <div class="form-group" v-if="!auth">
                                 <label for="exampleInputEmail1">Email address</label>
                                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="city" v-model="form.email">
                                 <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
@@ -49,33 +54,33 @@
                                 <div class="col-4">
                                     <div class="form-group">
                                         <label for="city">City</label>
-                                        <input type="text" class="form-control" id="city" placeholder="City..." name="city">
+                                        <input type="text" class="form-control" id="city" placeholder="City..." name="city" v-model="form.city">
                                     </div>
                                 </div>
 
                                 <div class="col-4">
                                     <div class="form-group">
-                                        <label for="city">Street</label>
-                                        <input type="text" class="form-control" id="street" placeholder="Street..." name="street">
+                                        <label for="street">Street</label>
+                                        <input type="text" class="form-control" id="street" placeholder="Street..." name="street" v-model="form.street">
                                     </div>
                                 </div>
 
                                 <div class="col-2">
                                     <div class="form-group">
                                         <label for="city">House</label>
-                                        <input type="number" class="form-control" id="house" placeholder="House..." name="house">
+                                        <input type="number" class="form-control" id="house" placeholder="House..." name="house" v-model="form.house">
                                     </div>
                                 </div>
 
                                 <div class="col-2">
                                     <div class="form-group">
                                         <label for="apartment">Apartment</label>
-                                        <input type="number" class="form-control" id="apartment" placeholder="Apartment..." name="apartment">
+                                        <input type="number" class="form-control" id="apartment" placeholder="Apartment..." name="apartment" v-model="form.apartment">
                                     </div>
                                 </div>
 
                             </div>
-                            <button type="submit" class="btn btn-primary">Make order</button>
+                            <button type="submit" class="btn btn-primary" @click.prevent="createOrder(form)">Make order</button>
                         </form>
                     </div>
                 </div>
@@ -98,19 +103,22 @@
                     street: null,
                     house: null,
                     apartment: null
-                }
+                },
+                isDollars: true
             }
         },
         computed: {
             ...mapGetters({
                 pizzas: 'card/card',
-                totalPrice: 'card/totalPrice'
+                totalPrice: 'card/totalPrice',
+                auth: 'auth/authenticated'
             })
         },
         methods: {
             ...mapActions({
                 removeFromCardAction: 'card/removeFromCard',
-                getFromLocalStorageAction: 'card/getFromLocalStorage'
+                getFromLocalStorageAction: 'card/getFromLocalStorage',
+                createOrderAction: 'order/createOrder'
             }),
 
             removeFromCard(pizza) {
@@ -119,11 +127,15 @@
 
             getFromLocalStorage() {
                 this.getFromLocalStorageAction()
+            },
+
+            createOrder(fields) {
+                this.createOrderAction(fields)
             }
         },
         mounted() {
             this.getFromLocalStorage()
-        }
+        },
     }
 </script>
 
--- resources/js/components/PizzasComponent.vue
+++ resources/js/components/PizzasComponent.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <button class="btn btn-secondary float-right" @click.prevent="isDollars = !isDollars">Change Currency</button>
         <h2 class="text-center text-secondary mb-5">Pizzas!</h2>
         <div class="row">
             <div class="col-6 col-sm-12 col-md-6 mb-3" v-for="pizza in pizzas">
@@ -7,7 +8,8 @@
                     <img class="pizza card-img" :src="`/storage/images/pizza-${pizza.id}.svg`">
                     <div class="card-body d-flex flex-column justify-content-center align-items-center">
                         <h4>{{ pizza.name }}</h4>
-                        <h5 class="text-primary">{{ pizza.price }} $</h5>
+                        <h5 class="text-primary" v-if="isDollars">{{ pizza.price }} $</h5>
+                        <h5 class="text-primary" v-else>{{ Math.ceil(pizza.price * 0.85) }} €</h5>
                         <p class="card-text text-muted">{{ pizza.description }}</p>
                         <button class="btn btn-primary" v-on:click="addToCard(pizza)">Add to card</button>
                     </div>
@@ -18,11 +20,13 @@
 </template>
 
 <script>
-    import { mapActions, mapGetters } from 'vuex';
+    import {mapActions, mapGetters} from 'vuex';
 
     export default {
         data() {
-            return {}
+            return {
+                isDollars: true
+            }
         },
         computed: {
             ...mapGetters({

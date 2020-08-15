8dfbbaa095530d9896123835c16e8726131e364e
kuzmich321 <dota2drot@gmail.com>
2020-08-15 15:15:36 +0000

    * vue cookies removed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,12 @@
 import Vue from 'vue';
 import router from './router/index';
 import App from './App';
-import VueCookies from 'vue-cookies';
 import store from './store/index';
 
 require('./bootstrap');
 require('./store/subscriber')
 
 axios.defaults.baseURL = 'https://pizza-project123.herokuapp.com/api';
-
-Vue.use(VueCookies);
 
 store.dispatch('auth/attempt', localStorage.getItem('token')).then(() => {
     new Vue({
